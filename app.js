a34c08e304cdf5d4131d7f95a585f0a3700d22b1
Iamsushantgautam <sushantkumarlko2507@gmail.com>
2025-05-04 09:24:57 +0530

    app update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const dotenv = require("dotenv");
+const MongoStore = require('connect-mongo');
 
 
 dotenv.config();
@@ -12,9 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.SESSION_SECRET || 'your_default_secret_key',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URI, // your MongoDB connection string
+    ttl: 14 * 24 * 60 * 60 // = 14 days
+  }),
+  cookie: { secure: false } // change to true if using HTTPS
 }));
 
 
