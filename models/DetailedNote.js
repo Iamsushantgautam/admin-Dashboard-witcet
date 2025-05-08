0d26815c666a4906a03232a10a0dd640fec5c0f4
Iamsushantgautam <sushantkumarlko2507@gmail.com>
2025-05-08 12:39:44 +0530

    up/down feature add

--- models/DetailedNote.js
+++ models/DetailedNote.js
@@ -3,8 +3,18 @@ const mongoose = require('mongoose');
 const unitSchema = new mongoose.Schema({
   unitTitle: String,
   ownerName: String,
-  pdfLink: String
+  pdfLink: {
+    type: String,
+    set: function(link) {
+      const match = link.match(/\/file\/d\/(.+?)\//);
+      if (match && match[1]) {
+        return `https://drive.google.com/uc?export=download&id=${match[1]}`;
+      }
+      return link; // If it's already formatted or not a Drive link
+    }
+  }
 });
+
 
 const detailedNoteSchema = new mongoose.Schema({
   notesTitle: String,
